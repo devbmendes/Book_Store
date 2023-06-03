89d02fc24e17e6f7e5b898630d870bae770379b0
valter correia <baldhuino@gmail.com>
2023-06-03 10:57:02 +0100

    Category class

--- src/main/java/com/devb/book_store/entity/Category.java
+++ src/main/java/com/devb/book_store/entity/Category.java
@@ -1,5 +1,38 @@
 package com.devb.book_store.entity;
 
 public class Category {
+	
+	private Integer id;
+	private Type type;
+	private String desc;
+	public Category(Integer id, Type type, String desc) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.desc = desc;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Type getType() {
+		return type;
+	}
+	public void setType(Type type) {
+		this.type = type;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public Category() {
+		
+	}
+	
+	
 
 }
---
+++ src/main/java/com/devb/book_store/entity/Type.java
@@ -0,0 +1,12 @@
+package com.devb.book_store.entity;
+
+public enum Type {
+	ADVENTURE_STORIES,
+	CRIME,
+	CLASSICS,
+	FANTASY,
+	HORROR,
+	HUMOUR,
+	CIENTIFIC
+
+}
