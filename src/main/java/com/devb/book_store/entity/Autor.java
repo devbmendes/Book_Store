c3b2056f7a8ac284a60ddafedb65c5a2cc771c69
valter correia <baldhuino@gmail.com>
2023-06-03 10:38:23 +0100

    Class author

---
+++ src/main/java/com/devb/book_store/entity/Autor.java
@@ -0,0 +1,78 @@
+package com.devb.book_store.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Autor {
+
+	private Integer id;
+	private String firstname;
+	private String lastname;
+	private String email;
+	
+	private List<Book> books = new ArrayList<>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Autor(Integer id, String firstname, String lastname, String email) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+
+	public Autor() {
+		
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
---
+++ src/main/java/com/devb/book_store/entity/Book.java
@@ -0,0 +1,5 @@
+package com.devb.book_store.entity;
+
+public class Book {
+
+}
