22b1459583dab54fa9f83f02215db82a5c6697de
valter correia <baldhuino@gmail.com>
2023-06-04 13:32:04 +0100

    Method Update by AuthorController

--- src/main/java/com/devb/book_store/controller/AuthorController.java
+++ src/main/java/com/devb/book_store/controller/AuthorController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +34,11 @@ public class AuthorController {
 	public ResponseEntity<Author> save(AuthorDTO authorDTO) {
 		Author author = authorService.save(authorDTO);
 		return ResponseEntity.status(HttpStatus.CREATED).body(author);
+	}
+	@PutMapping("/{id}")
+	public ResponseEntity<Author> update(@PathVariable Integer id,AuthorDTO authorDTO){
+		Author author = authorService.update(id, authorDTO);
+		return ResponseEntity.status(HttpStatus.OK).body(author);
 	}
 	
 
